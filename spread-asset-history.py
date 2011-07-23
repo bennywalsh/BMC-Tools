ed57ca1767ce3ea2feb4347ecb387a7269acadad
Ben Walsh <benny.walsh@gmail.com>
2011-07-23 15:00:00 +0100

    second commit

---
+++ spread-asset-history.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+import re
+import time
+
+if __name__ == '__main__':
+
+	proc = subprocess.Popen(['.' + os.sep + 'bmc.py', 'asset-history', '10000'],stdout=subprocess.PIPE,)
+	stdout_value = proc.communicate()[0]
+	stdout_value = stdout_value.replace('asset_id --:-- ', '')
+	stdout_value = stdout_value.replace('timestamp --:-- ', '')
+	stdout_value = stdout_value.replace('transaction --:-- ', '')
+	stdout_value = stdout_value.replace('type --:-- ', '')
+	stdout_value = stdout_value.replace('quantity --:-- ', '')
+	stdout_value = stdout_value.replace('\n', ';')
+	#stdout_value = stdout_value.replace(' ', '\n')
+	records = re.split(' ', stdout_value)
+
+	proper_records = []
+	for record in records:
+		items = re.split(';', record)
+		items[:] = [x for x in items if x != '']
+		proper_records.append(items)
+
+	del proper_records[0]
+
+	def blah(tup):
+		return tup[1]
+	
+	proper_records.sort(key=blah)
+
+	print 'Asset_id,Time,Transaction,Type,Quantity'
+
+	for proper_record in proper_records:
+		proper_record[1] = time.strftime("%d/%m/%Y %H:%M:%S", time.localtime(int(proper_record[1])))
+		print ','.join(proper_record)
+
+
+	#print stdout_value
+
